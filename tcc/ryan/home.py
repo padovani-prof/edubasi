61643e52473a9eb4eada2193920e85494dd42741
Ryan Pereira da Silva <166587298+RyanPSilvaa@users.noreply.github.com>
2025-10-01 19:22:50 -0400

    Update home.py

--- tcc/ryan/home.py
+++ tcc/ryan/home.py
@@ -1,161 +1,234 @@
-import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
+import plotly.express as px
 
-# Configuração da página
-st.set_page_config(page_title="Dashboard | Microdados", layout="wide")
-st.title("Dashboard | Microdados")
 
-# =-=-=-=-=-= Sidebar =-=-=-=-=-=
-st.sidebar.subheader('Procure os dados filtrados:')
-
-def parametros_sidebar():
-    ano = st.sidebar.selectbox("Selecione o ano:", ["2019", "2023", "Comparar"])
-    return ano
-
-# Carregando os dados filtrados
-@st.cache_data
+# Função para carregar dados
 def carregar_dados():
-    df2019 = pd.read_csv("MICRODADOS_ITA_2019_FILTRADO.csv")
-    df2023 = pd.read_csv("MICRODADOS_ITA_2023_FILTRADO.csv")
-    return df2019, df2023
+    df_2019 = pd.read_csv("MD_ITA_2019.csv", sep=";", encoding="latin1")
+    df_2023 = pd.read_csv("MD_ITA_2023.csv", sep=";", encoding="latin1")
+    return df_2019, df_2023
 
-df2019, df2023 = carregar_dados()
+df_2019, df_2023 = carregar_dados()
 
-# Função para adicionar números sobre barras
-def adicionar_valores(ax):
-    for bar in ax.patches:
-        ax.text(
-            bar.get_x() + bar.get_width()/2,           # centraliza horizontalmente
-            bar.get_height() + bar.get_height()*0.01,  # 1% acima da barra
-            int(bar.get_height()),                      # valor inteiro
-            ha='center', va='bottom'
+# Adiciona coluna de ano e combina os dados dos dois anos para análise
+df_2019["ANO"] = 2019
+df_2023["ANO"] = 2023
+df_final = pd.concat([df_2019, df_2023], ignore_index=True)
+
+# -=-=-=-=-=-=-=-=-=-
+# Criação da Barra Lateral
+# -=-=-=-=-=-=-=-=-=-
+st.sidebar.header("Filtros")
+
+#Seleção dos filtros dos anos de 2019 e 2023
+anos = st.sidebar.multiselect(
+    "Selecione o(s) ano(s):",
+    options=[2019, 2023],
+    default=[2019, 2023]
+)
+# Filtra os dados de acordo com os anos selecionados
+df_filtrado = df_final[df_final["ANO"].isin(anos)]
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Checkbox na barra lateral
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+incluir_sem_escola = st.sidebar.checkbox(
+    "Incluir alunos sem escola",
+    value=True,
+    help="Se desmarcado, alunos sem informação de escola não serão considerados."
+)
+
+if not incluir_sem_escola and "CO_MUNICIPIO_ESC" in df_filtrado.columns:
+    # Remove os alunos sem escola
+    df_filtrado = df_filtrado[df_filtrado["CO_MUNICIPIO_ESC"].notna()]
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Título e cabeçalho
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+st.title("Análise Social")
+st.header("Microdados do Exame Nacional do Ensino Médio")
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Criação das abas Dados Gerais, pessoais, escolares de renda, moradia, transporte e aparelhos e tecnologias em casa
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+tabs = st.tabs([
+    "Dados Gerais",
+    "Dados Pessoais",
+    "Depêndencia Administrativa",
+    "Dados de Renda",
+    "Dados de Moradia",
+    "Dados de Transporte",
+    "Aparelhos e Tecnologias Digitais"
+])
+
+#-=-=-=-=-=-=-=-=-=-
+# Dados gerais
+# --=-=-=-=-=-=-=-=-
+with tabs[0]:
+    df_inscritos = df_filtrado  # todos os inscritos, sem filtro
+    if not df_inscritos.empty:
+        contagem = (
+            # AGRUPA DADOS PELAS COLUNAS / Conta o número de linhas em cada grupo / Transforma o resultado em um novo DF com duas colunas de ano e quantidade
+            df_inscritos.groupby("ANO").size().reset_index(name="Quantidade")
         )
+        fig = px.bar(
+            contagem,
+            x="ANO",
+            y="Quantidade",
+            text="Quantidade",
+            title="Quantidade de inscritos no ENEM por ano"
+        )
+        st.plotly_chart(fig)
+    else:
+        st.warning("Não há dados para os anos selecionados.")
 
-# =-=-=-=-=-= Corpo =-=-=-=-=-=
-ano_escolhido = parametros_sidebar()
-if ano_escolhido == "2019":
-    st.header('Visualização dos Microdados de 2019')
-    df = df2019
+#-=-=-=-=
+# Dados pessoais
+# -=-=-=-=-=-=-=-
+with tabs[1]:
+    # pegando somente a coluna sexo
+    dados = df_filtrado["TP_SEXO"].dropna().unique()
+    # Opções de sexo disponíveis
+    sexos_selecionados = st.sidebar.multiselect(
+        "Sexo:",
+        options=dados,
+        default=dados
+    )
 
-    # Gráfico de inscritos 2019
-    st.subheader("Total de Inscritos (2019)")
-    inscritos_2019 = len(df)
-    fig, ax = plt.subplots(figsize=(5,5))
-    ax.bar(["2019"], [inscritos_2019])
-    adicionar_valores(ax)
-    ax.set_ylabel("Quantidade de inscritos")
-    st.pyplot(fig)
+    # filtrando os dados com o sexo escolhido masculino ou feminino
+    filtrado = df_filtrado[df_filtrado["TP_SEXO"].isin(sexos_selecionados)]
+    #substitui M e F por Masculino e feminino
+    if "TP_SEXO" in filtrado:
+        #troca os valores de TP_SEXO
+        filtrado["TP_SEXO"] = filtrado["TP_SEXO"].replace({
+            "M": "Masculino",
+            "F": "Feminino"
+        })
+    #criando o gráfico de pizza com os dados filtrados
+    graf_pizza = px.pie(
+        filtrado,
+        names="TP_SEXO",
+        hole=0.3,
+        title="Paticipantes por sexo"
+    )
+    st.plotly_chart(graf_pizza)
 
-    # Gráficos distribuição por sexo e idade
-    col1, col2 = st.columns(2)
+#-=-=-=-=-=-=-=-=-=-=-=-==-=
+# TIPO DE ESCOLA
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-
+with tabs[2]:
+    # opções de tipo de escola
+    dados = df_filtrado["TP_DEPENDENCIA_ADM_ESC"].dropna().unique()
+    escola_selecionada = st.sidebar.multiselect(
+        "Tipo de Escola:",
+        options=dados,
+        default=dados
+    )
+    # filtra os dados
+    filtrado_escola = df_filtrado[df_filtrado["TP_DEPENDENCIA_ADM_ESC"].isin(escola_selecionada)]
+    # conta os tipos de escola e padroniza os valores de 1,2,3 e 4 para o tipo de escola
+    if "TP_DEPENDENCIA_ADM_ESC" in filtrado_escola.columns:
+        filtrado_escola["TP_DEPENDENCIA_ADM_ESC"] = filtrado_escola["TP_DEPENDENCIA_ADM_ESC"].replace({
+            1: "Federal",
+            2: "Estadual",
+            3: "Municipal",
+            4: "Privada"
+        })
+    # muda na parte inferior do gráfico o nome TP_ESCOLA para dependência administrativa
+    contagem = (
+        filtrado_escola["TP_DEPENDENCIA_ADM_ESC"].value_counts().reset_index()
+    )
+    contagem.columns = ["Dependência administrativa", "Quantidade"]
+    #Grafico de barras
+    grafico = px.bar(
+        contagem,
+        x="Dependência administrativa",
+        y="Quantidade",
+        text="Quantidade",
+        title="Quantidade de inscritos por tipo de escola"
+    )
+    st.plotly_chart(grafico)
 
-    with col1:
-        # Distribuição por Sexo (2019) em formato de pizza
-        st.subheader("Distribuição por Sexo (2019)")
-        # Contagem de cada sexo
-        counts = df['TP_SEXO'].value_counts()
-        # Criar gráfico de pizza
-        fig, ax = plt.subplots(figsize=(6, 6))
-        ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=['#66b3ff', '#ff9999'])
-        ax.set_title('2019')
-        # Exibir gráfico
-        st.pyplot(fig)
+#=-=-=-=-=-=-=-=-=-=-=-=-=
+# Dados de Renda
+#=-=-=-=-=-=-=-=-=-=-=-=-=
+with tabs[3]:
+    # Mapeamento dos valores de Q006 para mudar as letras para o nome de A a Q
+    mapa_renda = {
+        "A": "Nenhuma renda",
+        "B": "Até R$ 1.320,00",
+        "C": "De R$ 1.320,01 até R$ 1.980,00",
+        "D": "De R$ 1.980,01 até R$ 2.640,00",
+        "E": "De R$ 2.640,01 até R$ 3.300,00",
+        "F": "De R$ 3.300,01 até R$ 3.960,00",
+        "G": "De R$ 3.960,01 até R$ 5.280,00.",
+        "H": "De R$ 5.280,01 até R$ 6.600,00.",
+        "I": "De R$ 6.600,01 até R$ 7.920,00",
+        "J": "De R$ 7.920,01 até R$ 9240,00",
+        "K": "De R$ 9.240,01 até R$ 10.560,00",
+        "L": "De R$ 10.560,01 até R$ 11.880,00",
+        "M": "De R$ 11.880,01 até R$ 13.200,00",
+        "N": "De R$ 13.200,01 até R$ 15.840,00",
+        "O": "De R$ 15.840,01 até R$19.800,00",
+        "P": "De R$ 19.800,01 até R$ 26.400,00",
+        "Q": "Acima de R$ 26.400,00"
 
-    with col2:
-        st.subheader("Distribuição por Idade (2019)")
-        fig, ax = plt.subplots(figsize=(6,5))
-        counts = df['TP_FAIXA_ETARIA'].value_counts()
-        counts.plot(kind='bar', ax=ax)
-        adicionar_valores(ax)
-        st.pyplot(fig)
+    }
+    # Substitui diretamente na coluna renomeando as letras pelas faixas de renda pelo meio do mapa_renda
+    df_filtrado["Q006"] = df_filtrado["Q006"].replace(mapa_renda)
+    # Para o multiselect, pega valores únicos já traduzidos
+    dados_renda = df_filtrado["Q006"].dropna().unique()
+    renda_selecionado = st.sidebar.multiselect(
+        "Renda",
+        options=dados_renda,
+        default=dados_renda
+    )
+    # Contagem dos dados filtrados
+    renda = df_filtrado["Q006"].value_counts().reset_index()
+    renda.columns = ["Dados de Renda", "Quantidade"]
+    # Gráfico Pizza de renda
+    graf_pizza = px.pie(
+        renda,
+        names="Dados de Renda",
+        values="Quantidade",
+        hole=0.3,
+        title="Dados de Renda dos Participantes"
+    )
+    st.plotly_chart(graf_pizza)
 
-    # Dados filtrados
-    st.header("Dados filtrados de 2019")
-    st.dataframe(df2019.sample(20))
+#-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Aba 4 - Dados de moradia
+#-=-=-=-=-=-=-=-=-=-=-=-=-=
+with tabs[4]:
+    moradia_dados = df_filtrado["Q005"].dropna().unique()
+    # Contagem dos dados filtrados
+    moradia = df_filtrado["Q005"].value_counts().reset_index()
+    grafico = px.pie(
+        moradia,
+        names="Q005",
+        values="Q005",
+        hole=0.3
+    )
+    st.plotly_chart(grafico)
 
-elif ano_escolhido == "2023":
-    st.header('Visualização dos Microdados de 2023')
-    df = df2023
+with tabs[5]:
+    st.subheader("Dados de Transporte - Carros")
+    veiculos_map = {
+        "A": "Não",
+        "B": "Sim, um",
+        "C": "Sim, dois",
+        "D": "Sim, três",
+        "E": "Sim, quatro ou mais"
+    }
 
-    # Gráfico de inscritos 2023
-    st.subheader("Total de Inscritos (2023)")
-    inscritos_2023 = len(df)
-    fig, ax = plt.subplots(figsize=(5,5))
-    ax.bar(["2023"], [inscritos_2023])
-    adicionar_valores(ax)
-    ax.set_ylabel("Quantidade de inscritos")
-    st.pyplot(fig)
+    # Mapear os códigos
+    df_filtrado["Q010"] = df_filtrado["Q010"].map(veiculos_map)
+    df_filtrado["Q011"] = df_filtrado["Q011"].map(veiculos_map)
 
-    # Gráficos distribuição por sexo e idade
-    col1, col2 = st.columns(2)
-
-    with col1:
-        # Distribuição por Sexo (2023) em formato de pizza
-        st.subheader("Distribuição por Sexo (2023)")
-        # Contagem de cada sexo
-        counts = df['TP_SEXO'].value_counts()
-        # Criar gráfico de pizza
-        fig, ax = plt.subplots(figsize=(6, 6))
-        ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=['#66b3ff', '#ff9999'])
-        ax.set_title('2023')
-        # Exibir gráfico
-        st.pyplot(fig)
-
-    with col2:
-        st.subheader("Distribuição por Idade (2023)")
-        fig, ax = plt.subplots(figsize=(6,5))
-        counts = df['TP_FAIXA_ETARIA'].value_counts()
-        counts.plot(kind='bar', ax=ax)
-        adicionar_valores(ax)
-        st.pyplot(fig)
-
-    # Dados filtrados
-    st.header("Dados filtrados de 2023")
-    st.dataframe(df2023.sample(20))
-
-else:  # Comparar
-    st.header("Comparação dos Microdados de 2019 e 2023")
-
-    # Comparação de inscritos
-    st.subheader("Total de Inscritos")
-    inscritos = pd.DataFrame({
-        "Ano": ["2019", "2023"],
-        "Inscritos": [len(df2019), len(df2023)]
-    })
-    fig, ax = plt.subplots(figsize=(7,5))
-    inscritos.plot(kind='bar', x='Ano', y='Inscritos', ax=ax, legend=False)
-    adicionar_valores(ax)
-    ax.set_ylabel("Quantidade de inscritos")
-    st.pyplot(fig)
-
-    # Comparação por sexo em formato de pizza
-    st.subheader("Distribuição por Sexo")
-    # Contagem de cada sexo
-    sexo2019 = df2019['TP_SEXO'].value_counts()
-    sexo2023 = df2023['TP_SEXO'].value_counts()
-    # Criar duas pizzas, uma para cada ano
-    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-    # Pizza 2019
-    axs[0].pie(sexo2019, labels=sexo2019.index, autopct='%1.1f%%', startangle=90, colors=['#66b3ff', '#ff9999'])
-    axs[0].set_title('2019')
-    # Pizza 2023
-    axs[1].pie(sexo2023, labels=sexo2023.index, autopct='%1.1f%%', startangle=90, colors=['#66b3ff', '#ff9999'])
-    axs[1].set_title('2023')
-    # Exibir gráfico
-    st.pyplot(fig)
-
-    # Comparação por idade
-    st.subheader("Distribuição por Idade")
-    idade2019 = df2019['TP_FAIXA_ETARIA'].value_counts()
-    idade2023 = df2023['TP_FAIXA_ETARIA'].value_counts()
-    idade_comparacao = pd.DataFrame({'2019': idade2019, '2023': idade2023}).fillna(0)
-    fig, ax = plt.subplots(figsize=(6,5))
-    idade_comparacao.plot(kind='bar', ax=ax)
-    adicionar_valores(ax)
-    st.pyplot(fig)
-
-    st.subheader("Dados filtrados de ambos os anos")
-    st.dataframe(df2019.sample(20))
-    st.dataframe(df2023.sample(20))
+    # Gráfico Carros
+    st.bar_chart(df_filtrado["Q010"].value_counts())
+    
+    st.subheader("Dados de Transporte - Motos")
+    # Gráfico Motos
+    st.bar_chart(df_filtrado["Q011"].value_counts())
